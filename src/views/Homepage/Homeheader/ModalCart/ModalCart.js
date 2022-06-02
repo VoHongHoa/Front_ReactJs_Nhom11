6c677a958b1e3ff003103c0def536d41aead52cc
VoHongHoa <19521522@gm.uit.edu.vn>
2022-06-02 07:44:00 +0700

    u

--- src/views/Homepage/Homeheader/ModalCart/ModalCart.js
+++ src/views/Homepage/Homeheader/ModalCart/ModalCart.js
@@ -101,7 +101,9 @@ class ModalCart extends Component {
               <div className="col-md-8">
                 <div className="card mb-4">
                   <div className="card-header py-3">
-                    <h5 className="mb-0">Cart - {allItems.length} items</h5>
+                    <h5 className="mb-0">
+                      Giỏ hàng - {allItems.length} sản phẩm
+                    </h5>
                   </div>
                   <div className="card-body">
                     {allItems &&
@@ -137,7 +139,7 @@ class ModalCart extends Component {
                                 <p>
                                   <strong>{item.title}</strong>
                                 </p>
-                                <p>Color: {item.color}</p>
+                                <p>Màu: {item.color}</p>
                                 <p>Ram: {item.ram}</p>
                                 <p>Rom: {item.rom}</p>
                                 <button
@@ -210,7 +212,7 @@ class ModalCart extends Component {
                 <div className="card mb-4">
                   <div className="card-body">
                     <p>
-                      <strong>Expected shipping delivery</strong>
+                      <strong>Thời gian giao hàng dự kiến</strong>
                     </p>
                     <p className="mb-0">12.10.2020 - 14.10.2020</p>
                   </div>
@@ -218,7 +220,7 @@ class ModalCart extends Component {
                 <div className="card mb-4 mb-lg-0">
                   <div className="card-body">
                     <p>
-                      <strong>We accept</strong>
+                      <strong>Chấp nhận thanh toán</strong>
                     </p>
                     <img
                       className="me-2"
@@ -244,23 +246,23 @@ class ModalCart extends Component {
               <div className="col-md-4">
                 <div className="card mb-4">
                   <div className="card-header py-3">
-                    <h5 className="mb-0">Summary</h5>
+                    <h5 className="mb-0">Bản tóm tắt hóa đơn</h5>
                   </div>
                   <div className="card-body">
                     <ul className="list-group list-group-flush">
                       <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                        Products
+                        Giá trị đơn hàng:
                         <span>{formatPrice(total)}</span>
                       </li>
                       <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                        Shipping
-                        <span>Gratis</span>
+                        Phí vận chuyển
+                        <span>Miễn phí</span>
                       </li>
                       <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                         <div>
-                          <strong>Total amount</strong>
+                          <strong>Tổng hóa đơn</strong>
                           <strong>
-                            <p className="mb-0">(including VAT)</p>
+                            <p className="mb-0">(Bao gồm thuế VAT)</p>
                           </strong>
                         </div>
                         <span>
@@ -274,7 +276,7 @@ class ModalCart extends Component {
                       className="btn btn-primary btn-lg btn-block"
                       onClick={() => this.handleSubmit(total)}
                     >
-                      Go to checkout
+                      Thanh toán
                     </button>
                   </div>
                 </div>
